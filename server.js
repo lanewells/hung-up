da179f047a1a59d075aa86fd1c1300da25cb29ef
lanewells <wellsdelaneyk@gmail.com>
2025-05-13 10:55:47 -0700

    Removed redundant mongoose connect in server.js.

--- server.js
+++ server.js
@@ -23,14 +23,6 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'client')))
 app.use('/assets', express.static(path.join(__dirname, 'client/assets')))
 
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err))
-
 app.get("/", async (req, res) => {
   res.send("Welcome to Hung-Up Backend")
 })
